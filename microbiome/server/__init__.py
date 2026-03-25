0282a61defa998b51b803ccfb7ad3f89a5c996e8
EnlightenedOmega <kumar24.roushan@gmail.com>
2026-03-25 22:14:12 +0530

    init

---
+++ microbiome/server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Microbiome environment server components."""
+
+from .microbiome_environment import MicrobiomeEnvironment
+
+__all__ = ["MicrobiomeEnvironment"]
---
+++ readme.md
@@ -0,0 +1,3 @@
+##Problem Statement
+Patients respond very differently to the same drug dose.
+Traditional pharmacology blames genetics, but genetics explains only a small portion of drug-response variability. The gut microbiome is now recognized as a major driver of this variability
